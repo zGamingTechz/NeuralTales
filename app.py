58467a4bd3fe68113a95b0c4d1794b0dc7a2c093
Bhavya Soni <rkumarbhavy@gmail.com>
2025-03-29 22:22:59 +0530

    Initial commit for app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+# Definitions
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+app.config['SECRET_KEY'] = keys.secret_key
+db = SQLAlchemy(app)
